6f0fb43de44b8484e96471e414eac55b03b50eac
TuongNghiBui <camellia.bui95@gmail.com>
2025-02-16 18:11:41 +0100

    feat: relate user to media

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,14 +2,35 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+  admin: {
+    useAsTitle: 'title',
+  },
   access: {
     read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
   },
   fields: [
     {
-      name: 'alt',
+      name: 'title',
       type: 'text',
       required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'owner',
+      type: 'relationship',
+      relationTo: 'users',
+      required: true,
+    },
+    {
+      name: 'uploadedAt',
+      type: 'date',
+      defaultValue: new Date(),
     },
   ],
   upload: true,
